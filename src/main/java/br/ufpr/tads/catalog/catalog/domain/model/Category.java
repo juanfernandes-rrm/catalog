2783f57d3f61ca8dfde26e5e7f2b123ac4f90b2f
Juan Fernandes <62907327+juanfernandes-rrm@users.noreply.github.com>
2024-10-13 15:39:28 -0300

    Catal 008 add liquibase (#6)
    * CATAL-008 Add liquibase
    
    * CATAL-008 Insert categories

--- src/main/java/br/ufpr/tads/catalog/catalog/domain/model/Category.java
+++ src/main/java/br/ufpr/tads/catalog/catalog/domain/model/Category.java
@@ -3,16 +3,23 @@ package br.ufpr.tads.catalog.catalog.domain.model;
 import jakarta.persistence.*;
 import lombok.Data;
 
-import java.util.UUID;
-
 @Data
 @Entity
 @Table(name = "CATEGORY")
 public class Category {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID id;
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "NAME")
     private String name;
+
+    @Column(name = "DESCRIPTION")
+    private String description;
+
+    @Column(name = "URL_IMAGE")
+    private String urlImage;
 
 }
