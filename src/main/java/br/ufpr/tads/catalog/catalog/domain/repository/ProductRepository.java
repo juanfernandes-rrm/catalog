06c08c48b6ef3311be6c5ea751c2373deb72131d
Juan Fernandes <juanfernandesrrm@gmail.com>
2024-11-05 21:14:00 -0300

    CATAL-009 Fix changeset, and add get products details

--- src/main/java/br/ufpr/tads/catalog/catalog/domain/repository/ProductRepository.java
+++ src/main/java/br/ufpr/tads/catalog/catalog/domain/repository/ProductRepository.java
@@ -1,6 +1,7 @@
 package br.ufpr.tads.catalog.catalog.domain.repository;
 
 import br.ufpr.tads.catalog.catalog.domain.model.Product;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -16,4 +17,6 @@ public interface ProductRepository extends JpaRepository<Product, UUID> {
     Optional<Product> findByCode(String code);
 
     Slice<Product> findByNameContainingIgnoreCase(String name, Pageable pageable);
+
+    Page<Product> findAllByIdIn(List<UUID> productIdList, Pageable pageable);
 }
